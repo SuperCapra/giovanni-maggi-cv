10fb89171574a0d5d2933d794e6fe5111a41e845
Giovanni Maggi <gvnnmaggi@gmail.com>
2022-07-16 18:52:56 +0200

    some test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import logo from './faccioneWhited.png';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="avatar" />
-        <p>
-          Ciao sono <code>Giovanni</code> e questa sarà la mia pagina :)
-        </p>
-        <p>Stay tuned for more</p>
-      </header>
-    </div>
+    <Homepage/>
   );
+}
+
+class Homepage extends React.Component{
+  return42() {
+    return '42'
+  }
+
+  renderNumber(i) {
+    return(
+      <p>{i}</p>
+    )
+  }
+
+  render() {
+    return (    
+      <div className="App">
+        <header className="App-header">
+          <div class="wrapper-image">
+            <img src={logo} className="App-logo" alt="avatar" />
+          </div>
+          <p>
+            Ciao sono <code>Giovanni Maggi</code> e questa sarà la mia pagina :)
+          </p>
+          <p>Stay tuned for more</p>
+          {this.return42()}
+          {this.renderNumber(9)}
+          <Test/>
+        </header>
+      </div>
+    )
+  }
+}
+
+class Test extends React.Component{
+  render() {
+    return(<p>42</p>)
+  }
 }
 
 export default App;
