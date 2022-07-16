9b703f3e4abaa03d73c5328fb0eeaaebc080da9f
Giovanni Maggi <gvnnmaggi@gmail.com>
2022-07-16 17:21:31 +0200

    added image as avatar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './faccioneWhited.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="avatar" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ciao sono <code>Giovanni</code> e questa sarà la mia pagina :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Stay tuned for more</p>
       </header>
     </div>
   );
